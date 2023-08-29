514e1cf992008e52735aa7a4522c021225d47e15
LandonS <71854345+KudamonoHakka@users.noreply.github.com>
2023-08-29 15:17:57 -0700

    Theta error calculation
    Produce mapped delta theta values; these will eventually become the labels we associate with outputted data

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include "init.h"
 #include "agent.h"
 
+void simulate(AGENT* agent, PIXEL_LINK* sortedHead, unsigned char* pixelData, int width, int height);
+double errorCalculate(double deltaTheta);
+
 int main() {
   // Read the BMP file; extract pixels and size
   int width;
@@ -25,6 +28,26 @@ int main() {
   agent->xViewRange = 25;
   agent->yViewRange = 10;
 
+  simulate(agent, sortedHead, pixelData, width, height);
+
+  // Do a final cleanup
+  cleanup(sortedHead);
+  free(agent);
+
+  return 0;
+}
+
+double errorCalculate(double deltaTheta)
+{
+  // This function produces a label in relationship to deltaTheta
+  deltaTheta -= (deltaTheta > 180.0)? 360 : 0;
+
+  // Map our deltaTheta (0, 90) to a sinusoidal function; if we exceed an absolute deltaTheta greater than 90, assume the value to be max turning
+  return (abs(deltaTheta) < 90)? -sin(degreeRadConvert(deltaTheta, 0)): abs(deltaTheta) / deltaTheta;
+}
+
+void simulate(AGENT* agent, PIXEL_LINK* sortedHead, unsigned char* pixelData, int width, int height)
+{
   double* aScreen;
   for (PIXEL_LINK* pl = sortedHead; pl->nextPixel != 0x0; pl = pl->nextPixel)
   {
@@ -34,7 +57,7 @@ int main() {
     while (abs(deltaTheta) > 4.0)
     {
       aScreen = screenshot(agent, pixelData, width, height);
-      printViewport(agent, aScreen);
+      //printViewport(agent, aScreen);
       free(aScreen);
 
 
@@ -48,12 +71,11 @@ int main() {
 
       double turnDirection = (deltaTheta > 180)? 1.0 : -1.0;
 
-      printf("Turn %c\n\n", ((turnDirection == 1.0)? 'R' : 'L'));
+      printf("Turn %c: %.2f\n\n", ((turnDirection == 1.0)? 'R' : 'L'), errorCalculate(deltaTheta));
 
       agent->angle += agent->rotStepSize * turnDirection;
       //printf("A\n");
     }
-
 
     //agent->angle = 360 - twoPointAngle(pl->xPos, pl->yPos, pl->nextPixel->xPos, pl->nextPixel->yPos) + 90;
     agent->angle = twoPointAngle(pl->xPos, pl->yPos, pl->nextPixel->xPos, pl->nextPixel->yPos);
@@ -67,10 +89,10 @@ int main() {
       agent->yPos -= (int)((sin(degreeRadConvert(360 - agent->angle + 90, 0)) * agent->stepSize));
 
       aScreen = screenshot(agent, pixelData, width, height);
-      printViewport(agent, aScreen);
+      //printViewport(agent, aScreen);
       free(aScreen);
 
-      printf("Go forward %.2f; (%.2f, %.2f); distance: %.2f\n\n", agent->angle, cos(degreeRadConvert(360 - agent->angle + 90, 0)), sin(degreeRadConvert(360 - agent->angle + 90, 0)), sqrt(pow(agent->xPos - pl->nextPixel->xPos, 2) + pow(agent->yPos - pl->nextPixel->yPos, 2)));
+      //printf("Go forward %.2f; (%.2f, %.2f); distance: %.2f\n\n", agent->angle, cos(degreeRadConvert(360 - agent->angle + 90, 0)), sin(degreeRadConvert(360 - agent->angle + 90, 0)), sqrt(pow(agent->xPos - pl->nextPixel->xPos, 2) + pow(agent->yPos - pl->nextPixel->yPos, 2)));
       //printf("%.2f\n", sqrt(pow(agent->xPos - pl->xPos, 2) + pow(agent->yPos - pl->yPos, 2)));
 
     }
@@ -81,20 +103,5 @@ int main() {
 
     printf("\n");
   }
-
-  /*
-  agent->xPos += (int)(cos(degreeRadConvert(agent->angle, 0)) * agent->stepSize);
-  agent->yPos -= (int)(sin(degreeRadConvert(agent->angle, 0)) * agent->stepSize);
-
-  printf("\n%.2f\n",agent->angle);
-
-  aScreen = screenshot(agent, pixelData, width, height);
-  printViewport(agent, aScreen);*/
-
-  // Do a final cleanup
-  cleanup(sortedHead);
-  //free(aScreen);
-  free(agent);
-
-  return 0;
+  free(aScreen);
 }
