3f508af3b1ecdd3cd27a7a42c0e21ce6da6b5ae8
LandonS <71854345+KudamonoHakka@users.noreply.github.com>
2023-08-28 18:14:34 -0700

    Camera Rotation
    Finally we can rotate a camera and get a local screenshot

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 double twoPointAngle(int xPos1, int yPos1, int xPos2, int yPos2);
 double* screenshot(AGENT* agent, unsigned char* pixelData, int width, int height);
+double degreeRadConvert(double inp, char isRad);
 
 int main() {
   // Read the BMP file; extract pixels and size
@@ -21,14 +22,17 @@ int main() {
 
   agent->xPos = sortedHead->xPos;
   agent->yPos = sortedHead->yPos;
-  agent->angle = twoPointAngle(sortedHead->xPos, sortedHead->yPos, sortedHead->nextPixel->xPos, sortedHead->nextPixel->yPos);
+  //agent->angle = twoPointAngle(sortedHead->xPos, sortedHead->yPos, sortedHead->nextPixel->xPos, sortedHead->nextPixel->yPos);
+  agent->angle = 45;
 
-  agent->xViewRange = 20;
-  agent->yViewRange = 10;
+  //agent->xViewRange = 2;
+  //agent->yViewRange = 1;
 
-  //agent->xViewRange = 100;
-  //agent->yViewRange = 50;
+  //agent->xViewRange = 20;
+  //agent->yViewRange = 10;
 
+  agent->xViewRange = 100;
+  agent->yViewRange = 50;
 
   double* test = screenshot(agent, pixelData, width, height);
 
@@ -42,6 +46,7 @@ int main() {
     }
     printf("\n");
   }
+
 
   /*
   for (PIXEL_LINK* pl = sortedHead; pl->nextPixel != 0x0; pl = pl->nextPixel)
@@ -67,14 +72,45 @@ double* screenshot(AGENT* agent, unsigned char* pixelData, int width, int height
   {
     for (int x = -agent->xViewRange; x <= agent->xViewRange; x++)
     {
-      int index = ((agent->yPos + y) * width + (agent->xPos + x)) * 3;
+      // Get relative angle of point; then add agent angle to find new angle
+
+      double angle = 180 - twoPointAngle(0, 0, x, y) - 90;
+
+      double distance = sqrt(pow(x, 2) + pow(y, 2));
+
+      angle = (angle < 0)? angle+360 : angle;
+      angle -= agent->angle;
+
+
+      //printf("(%d, %d): %.2f\n", x, y, angle);
+
+      // Get relative X and Y positions based off of calculated angle
+      double xChange = cos(degreeRadConvert(angle, 0))*distance;
+      double yChange = -sin(degreeRadConvert(angle, 0))*distance;
+
+      // Convert above values to whole number relative positions
+      int relX = (int)(xChange + 0.5*((xChange >= 0)?1.0:-1.0));
+      int relY = (int)(yChange + 0.5*((yChange >= 0)?1.0:-1.0));
+
+      //printf("(%d, %d)\n", x, y);
+      //printf("(%d, %d): %.2f\n", relX, relY, angle);
+      //printf("(%d, %d): %.2f\n", (int)(cos(degreeRadConvert(angle, 0))*distance+0.5), (int)(sin(degreeRadConvert(angle, 0))*distance+0.5), angle);
+
+      // With calculated index, populate viewBuffer
+      int index = ((agent->yPos + relY) * width + (agent->xPos + relX)) * 3;
       int viewBufferIndex = (agent->yViewRange + y) * (agent->xViewRange*2+1) + (agent->xViewRange + x);
-      //printf("(%d, %d) ", (agent->xPos + x), (agent->yPos + y));
-      //printf("[%d] ", ((agent->yViewRange + y) * (agent->xViewRange*2+1) + (agent->xViewRange + x)));
+
+      //printf("%d        %d\n", viewBufferIndex, index);
       viewBuffer[viewBufferIndex] = pixelData[index];
+
     }
   }
   return viewBuffer;
+}
+
+double degreeRadConvert(double inp, char isRad)
+{
+  return (isRad)? inp / M_PI * 180.0 : inp / 180.0 * M_PI;
 }
 
 double twoPointAngle(int xPos1, int yPos1, int xPos2, int yPos2)
@@ -83,7 +119,7 @@ double twoPointAngle(int xPos1, int yPos1, int xPos2, int yPos2)
   double theta =  atan2(yPos1 - yPos2, xPos1 - xPos2) - M_PI / 2.0;
 
   // Return the value in degrees
-  theta = theta / M_PI * 180.0;
+  theta = degreeRadConvert(theta, 1);
 
   // Don't spit a negative result
   return (theta >= 0)? theta : theta + 360;
